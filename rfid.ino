cd7e61ad47efaebae193c74ff27400823045c718
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2021-01-11 15:53:49 +0100

    Update rfid.ino

--- rfid.ino
+++ rfid.ino
@@ -12,7 +12,7 @@ MFRC522 rfid(SS_PIN, RST_PIN);
 byte nuidPICC[4];
 bool goodTag = false;
 String entry ="";
-
+int value = 0;
 LiquidCrystal lcd(7,6,5,4,3,2);
 void setup() {
   Serial.begin(9600);
@@ -22,8 +22,7 @@ void setup() {
   SPI.begin(); 
   
   // Init MFRC522 
-  rfid.PCD_Init(); 
-
+  rfid.PCD_Init();
 }
 
 void loop() {
@@ -48,40 +47,46 @@ void loop() {
     lcd.setCursor(0, 0);
     lcd.print("      RIP       ");
   }
-  // Initialisé la boucle si aucun badge n'est présent 
-  if ( !rfid.PICC_IsNewCardPresent())
-    return;
-
-  // Vérifier la présence d'un nouveau badge 
-  if ( !rfid.PICC_ReadCardSerial())
-    return;
-
-  // Enregistrer l'ID du badge (4 octets) 
-  for (byte i = 0; i < 4; i++) 
-  {
-    nuidPICC[i] = rfid.uid.uidByte[i];
-  }
-
-  if(nuidPICC[0]==134)
-    if(nuidPICC[1]==58)
-      if(nuidPICC[2]==209)
-        if(nuidPICC[3]==92)
-          goodTag = true;
-  // Re-Init RFID
-  rfid.PICC_HaltA(); // Halt PICC
-  rfid.PCD_StopCrypto1(); // Stop encryption on PCD
   
+  if(entry == "3.0" || entry == "4.0" )
+  {
 
-   if (goodTag == true)
-   {
-       Serial.println("warning");
-       goodTag = false;
+    // Initialisé la boucle si aucun badge n'est présent 
+    if ( !rfid.PICC_IsNewCardPresent())
+      return;
+  
+    // Vérifier la présence d'un nouveau badge 
+    if ( !rfid.PICC_ReadCardSerial())
+      return;
+  
+    // Enregistrer l'ID du badge (4 octets) 
+    for (byte i = 0; i < 4; i++) 
+    {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+  
+    if(nuidPICC[0]==134)
+      if(nuidPICC[1]==58)
+        if(nuidPICC[2]==209)
+          if(nuidPICC[3]==92)
+            goodTag = true;
+    // Re-Init RFID
+    rfid.PICC_HaltA(); // Halt PICC
+    rfid.PCD_StopCrypto1(); // Stop encryption on PCD
 
-   }
-   else {
+  }
+  
+  if (goodTag == true)
+  {
+    Serial.println("warning");
+  
+  }
+  else {
       Serial.println("ok");
-   }
-   delay(1000);
+      goodTag = false;
+
+  }
+  delay(1000);
 
    
 }
